394a481e44a6eabd324040edd413466691fd63ff
farmershao <farmershao@gmail.com>
2018-10-17 18:55:25 +0800

    spring-cloud-eureka客户端

--- stock-member/src/main/java/com/farmershao/www/stockmember/StockMemberApplication.java
+++ stock-member/src/main/java/com/farmershao/www/stockmember/StockMemberApplication.java
@@ -2,8 +2,11 @@ package com.farmershao.www.stockmember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication()
+@EnableEurekaClient
+@SpringBootApplication
 public class StockMemberApplication {
 
 	public static void main(String[] args) {
