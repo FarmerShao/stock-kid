14ba8d784e9290b36a8036b3419d427d13fd5b18
farmershao <farmershao@gmail.com>
2018-10-17 23:38:00 +0800

    springCloud集群

--- stock-member/src/main/java/com/farmershao/www/stockmember/StockMemberApplication.java
+++ stock-member/src/main/java/com/farmershao/www/stockmember/StockMemberApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+/**
+ * @EnableDiscoveryClient 与 @EnableEurekaClient 的区别：
+ * @EnableEurekaClient ：只是Eureka 的服务中心的客户端
+ * @EnableDiscoveryClient ： 服务中心的客户端，不仅仅局限于Eureka。
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StockMemberApplication {
 
